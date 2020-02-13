cdd9cd1c33d7951f8b0e384657dfe96d97a66684
hasancelik <hasan@hazelcast.com>
2020-02-13 16:34:00 +0300

    fix instance creation at NearCacheClientSupport

--- clients/client-near-cache/src/main/java/com/hazelcast/examples/NearCacheClientSupport.java
+++ clients/client-near-cache/src/main/java/com/hazelcast/examples/NearCacheClientSupport.java
@@ -4,6 +4,8 @@ import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 
+import static com.hazelcast.core.Hazelcast.newHazelcastInstance;
+
 abstract class NearCacheClientSupport extends NearCacheSupport {
 
     protected static HazelcastInstance initCluster() {
@@ -12,9 +14,12 @@ abstract class NearCacheClientSupport extends NearCacheSupport {
     }
 
     protected static HazelcastInstance[] initCluster(int clusterSize) {
-        serverInstance = Hazelcast.newHazelcastInstance();
-
         HazelcastInstance[] instances = new HazelcastInstance[clusterSize];
+        for (int i = 0; i < clusterSize; i++) {
+            instances[i] = newHazelcastInstance();
+            serverInstance = instances[i];
+        }
+
         for (int i = 0; i < clusterSize; i++) {
             instances[i] = HazelcastClient.newHazelcastClient();
         }
